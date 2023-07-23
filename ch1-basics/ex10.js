b02c7195314a9265ee5f3c4f514a11ec3572fc19
John Sison <jlsison777@gmail.com>
2023-07-23 06:29:56 -0400

    exercises from 9 to 12

---
+++ ch1-basics/ex10.js
@@ -0,0 +1,5 @@
+// What value does the following expression evaluate to?
+
+'foo' === 'Foo';
+
+//It evaluates as false since case matters when comparing strings.
---
+++ ch1-basics/ex11.js
@@ -0,0 +1,8 @@
+//What value does the following expression evaluate to?
+
+parseInt('3.1415');
+
+// It evaluates to 3. parseInt converts strings that begin with a digit to 
+// their numeric equivalent. If it encounters a non-digit such as ., it 
+// stops converting. Thus, this example converts the 3 at the beginning 
+// of the string but ignores everything else.
